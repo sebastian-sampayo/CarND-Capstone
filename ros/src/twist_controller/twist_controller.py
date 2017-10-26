96191d8121c0b24e90e41ef2295769af62edb003
peter <peterlai_0618@outlook.com>
2017-10-27 01:12:41 +0800

    fix controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -12,14 +12,13 @@ class Controller(object):
                                 kwargs.get('d', 0.1))        
         self.ctr_steering = YawController(kwargs.get('wheel_base'),
                                           kwargs.get('steer_ratio'),
-                                          kwargs.get('min_speed'), 
+                                          kwargs.get('min_speed', 0.0), 
                                           kwargs.get('max_lat_accel'),
                                           kwargs.get('max_steer_angle'))
 
     def control(self, cte, sample_time):
         # TODO: Change the arg, kwarg list to suit your needs
         err_throttle = self.pid_throttle.step(cte, sample_time)
-        err_steering = self.pid_steering.step(cte, sample_time)
         # Return throttle, brake, steer
         return 1.0 - abs(err_throttle), 0.0, -err_steering 
 
